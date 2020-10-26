3f486e4be10c255e616a007285cc2e2e754ca8ab
Кулик Дмитрий Сергеевич <Kulik.DS1@partner.ru>
2020-10-27 09:49:37 +1000

    add snackbar and empty task screen

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -33,83 +33,84 @@ class _MainPageState extends State<MainPage> {
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         color: Theme.of(context).primaryColor,
-        child: ListView.builder(
-          itemCount: tasksList.length,
-          itemBuilder: (context, index) {
-            var task = tasksList[index];
-            return Column(
-              children: [
-                GestureDetector(
-                  onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => TaskPage(),
-                      settings: RouteSettings(
-                        arguments: task,
-                      ),
-                    ),
-                  ),
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10)),
-                    child: Row(
-                      children: [
-                        Checkbox(
-                          value: task.isComplete,
-                          activeColor: Color.fromRGBO(98, 2, 238, 1),
-                          onChanged: (value) {
-                            setState(() => task.isComplete = value);
-                          },
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              task.title,
-                              style: TextStyle(fontSize: 18),
+        child: tasksList.isEmpty
+            ? Center(
+                child: Text(
+                'На данный момент задач нет',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 30, color: Colors.grey[700]),
+              ))
+            : ListView.builder(
+                itemCount: tasksList.length,
+                itemBuilder: (context, index) {
+                  var task = tasksList[index];
+                  return Column(
+                    children: [
+                      GestureDetector(
+                        onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => TaskPage(),
+                            settings: RouteSettings(
+                              arguments: task,
                             ),
-                            task.maxSteps == 0
-                                ? Container()
-                                : Text(
-                                    '${task.currentStep} из ${task.maxSteps}',
-                                    style: TextStyle(
-                                        fontSize: 16, color: Colors.grey[600]),
-                                  ),
-                          ],
-                        ),
-                        Spacer(
-                          flex: 1,
-                        ),
-                        IconButton(
-                          icon: Icon(
-                            Icons.delete,
-                            color: Color.fromRGBO(98, 2, 238, 1),
                           ),
-                          onPressed: () {
-                            final snackBar = SnackBar(
-                              content: Text('Функционал в разработке'),
-                              action: SnackBarAction(
-                                label: 'Ок',
-                                onPressed: () {},
-                              ),
-                            );
-                            Scaffold.of(context).showSnackBar(snackBar);
-                          },
                         ),
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  height: 5,
-                )
-              ],
-            );
-          },
-        ),
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Row(
+                            children: [
+                              Checkbox(
+                                value: task.isComplete,
+                                activeColor: Color.fromRGBO(98, 2, 238, 1),
+                                onChanged: (value) {
+                                  setState(() => task.isComplete = value);
+                                },
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    task.title,
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                  task.maxSteps == 0
+                                      ? Container()
+                                      : Text(
+                                          '${task.currentStep} из ${task.maxSteps}',
+                                          style: TextStyle(
+                                              fontSize: 16,
+                                              color: Colors.grey[600]),
+                                        ),
+                                ],
+                              ),
+                              Spacer(
+                                flex: 1,
+                              ),
+                              IconButton(
+                                icon: Icon(
+                                  Icons.delete,
+                                  color: Color.fromRGBO(98, 2, 238, 1),
+                                ),
+                                onPressed: () {
+                                  setState(() => tasksList.removeAt(index));
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 5,
+                      )
+                    ],
+                  );
+                },
+              ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.cyan[600],
